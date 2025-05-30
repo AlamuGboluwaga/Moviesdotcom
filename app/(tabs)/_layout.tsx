fe3ca3da800415a8003f7c9a69b0dcc9e503fe3f
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2025-05-30 16:43:10 +0100

    home tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,36 @@
-import { View, Text,Image} from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-import homeicon from '../../assets/images/home.png'
+import homeicon from "../../assets/images/home.png";
 
-const TabIcon =()=>{
-  return<>
-<View className="h-6 w-6 bg-red-600">
-  <Image source={homeicon}
-resizeMode="contain"
-// tintColor={'gray'}
 
-/>
-</View>
-  </>
-} 
+  
+   
+   const TabIcon = () => {
+     return (
+      <View>
+      <Image source={homeicon}/>
+      </View>
+     )
+   }
+   
+
+   
+  
 
 const TabLayout = () => {
-
   return (
     <Tabs>
       <Tabs.Screen
-        name="home"
+        name="hom"
         options={{
           headerShown: false,
           title: "Home",
-          tabBarIcon: ({focused,color,size})=>(
-            <TabIcon  />
-          ),
-          tabBarInactiveTintColor:'gray'
-        
+          tabBarIcon: ({ focused, color, size }) =>(
+            <TabIcon/>
+          )
+
+           
         }}
       />
     </Tabs>
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import homeicon from "../../assets/images/home.png";
 const Home = () => {
   return (
-    <View>
-      <Text className='text-red-500'>Home</Text>
+    <View className="h-screen w-full flex items-center justify-center">
+      {/* <View className="h-8 w-8 p-2 border relative"> */}
+        <Image source={homeicon} resizeMode="stretch" height={10} width={10} className="" />
+      {/* </View> */}
+      <Text className="text-red-500">Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
