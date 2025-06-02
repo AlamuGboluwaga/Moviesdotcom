2bd47b7536b1190d0054a8b0e97cd37580d920ef
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2025-06-02 10:06:02 +0100

    tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,18 +2,28 @@ import { Tabs } from "expo-router";
 import React from "react";
 import { Image, View } from "react-native";
 import homeicon from "../../assets/images/home.png";
+import Createicon from "../../assets/images/create.png";
+import profileicon from "../../assets/images/profile.png";
+import Savedicon from "../../assets/images/saved.png";
 
-interface TabIconProps{
-  focus:boolean
+interface TabIconProps {
+  focus: boolean;
+  icon: any;
 }
+const tabArray: { name: string; tabTitle: string; icon: any }[] = [
+  { name: "home", tabTitle: "Home", icon: homeicon },
+  { name: "create", tabTitle: "Create", icon: Createicon },
+  { name: "profile", tabTitle: "Profile", icon: profileicon },
+  { name: "saved", tabTitle: "Saved", icon: Savedicon },
+];
 
-const TabIcon = ({focus}:TabIconProps) => {
+const TabIcon = ({ focus, icon }: TabIconProps) => {
   return (
     <View className="h-6 w-6 ">
       <Image
-        source={homeicon}
+        source={icon}
         style={{ width: 24, height: 24 }}
-        tintColor={ focus ?"":'gray'}
+        tintColor={focus ? "" : "gray"}
       />
     </View>
   );
@@ -22,24 +32,19 @@ const TabIcon = ({focus}:TabIconProps) => {
 const TabLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen
-        name="home"
-        options={{
-          headerShown: false,
-          title: "Home",
-          tabBarIcon: ({ focused, color }) => <TabIcon  focus={focused}/>,
-          // tabBarInactiveTintColor:""
-        }}
-      />
+      {tabArray.map((item, index) => (
         <Tabs.Screen
-        name="saved"
-        options={{
-          headerShown: false,
-          title: "Saved",
-          tabBarIcon: ({ focused, color }) => <TabIcon  focus={focused}/>,
-          // tabBarInactiveTintColor:""
-        }}
-      />
+          key={index}
+          name={item.name}
+          options={{
+            headerShown: false,
+            title: item.tabTitle,
+            tabBarIcon: ({ focused, color }) => (
+              <TabIcon focus={focused} icon={item.icon} />
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   );
 };
