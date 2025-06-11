43569a09fe772914b254240e66e0f1cec8d0967b
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2025-06-11 08:56:21 +0100

    onbording page button

--- app/index.tsx
+++ app/index.tsx
@@ -9,8 +9,6 @@ import onboardimage from "../assets/images/onboard.png";
 import "../global.css";
 import Button from "@/components/Button";
 
-
-
 const index = () => {
   return (
     <Layout gap="gap-2">
@@ -39,22 +37,35 @@ const index = () => {
         <Text className="text-message font-extrabold  text-4xl ">
           Possibilities with <Text className="text-grad1  ">Aora</Text>
         </Text>
-        <Images source={curf}
-        resizeMode="contain"
-        h="h-30"
-        w="w-20"
-        otherClasses="absolute  top-14 right-14 "></Images>
+        <Images
+          source={curf}
+          resizeMode="contain"
+          h="h-30"
+          w="w-20"
+          otherClasses="absolute  top-14 right-14 "
+        ></Images>
       </Message>
-      <Message  h="h-24"
+      <Message
+        h="h-24"
         w="w-full"
-        otherClasses="flex items-center justify-center ">
-        <Text className="text-message ">Where Creativity Meets Innovation: Embark on</Text>
-         <Text className="text-message "> a Journey of Limitless Exploration with Aora</Text>
+        otherClasses="flex items-center justify-center "
+      >
+        <Text className="text-message ">
+          Where Creativity Meets Innovation: Embark on
+        </Text>
+        <Text className="text-message ">
+          {" "}
+          a Journey of Limitless Exploration with Aora
+        </Text>
       </Message>
-      <Button h='h-16' w='w-100' otherClasses="flex justify-center items-center bg-orange-400 rounded-lg " >
-        
-    <Text className="text-primary font-semibold text-xl">Continue with Email</Text>
-        
+      <Button
+        h="h-20"
+        w="w-[28rem]"
+        otherClasses="flex justify-center items-center bg-orange-40  "
+      >
+        <Text className="text-primary font-semibold text-xl rounded-lg">
+          Continue with Email
+        </Text>
       </Button>
     </Layout>
   );
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,26 +1,30 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { ButtonProps } from './constants';
-import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { ButtonProps } from "./constants";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Button = ({
   h,
   w,
   children,
-  
+
   otherClasses,
   linearGradientProps,
   ...props
 }: ButtonProps) => {
   return (
-    <TouchableOpacity className={`${h} ${w} ${otherClasses}`} {...props}>
+    <TouchableOpacity
+    className={`h-20  bg-[#FFA300] rounded-lg  ${h} ${w} ${otherClasses}`}
+      {...props}
+    >
       <LinearGradient
-        start={[1,0]}
-        end={[1, 1]}
-        colors={['#FF8C00', '#FFA300']}
+        start={[0, 1]}
+        end={[1, 0]}
+        colors={["#FF8C00", "#FFA300"]}
         {...linearGradientProps}
+        className=" h-20 w-[27rem] rounded-lg flex justify-center items-center"
       >
-       {children}
+        {children}
       </LinearGradient>
     </TouchableOpacity>
   );
